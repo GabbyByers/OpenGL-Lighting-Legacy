3b95f427755702fba1eae82de6245090407c46f5
♡ gabby ♡ <81900622+GabrielBuckingham@users.noreply.github.com>
2023-11-09 13:40:12 -0800

    Removed Comments

--- VAO.h
+++ VAO.h
@@ -6,15 +6,12 @@
 
 class VAO {
 public:
-	// ID reference for the Vertex Array Object
 	GLuint ID;
 
-	// Constructor that generates a VAO ID
 	VAO() {
 		glGenVertexArrays(1, &ID);
 	}
 
-	// Links a VBO to the VAO using a certain layout
 	void LinkVBO(VBO& VBO, GLuint layout) {
 		VBO.Bind();
 		glVertexAttribPointer(layout, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
@@ -22,17 +19,14 @@ public:
 		VBO.Unbind();
 	}
 
-	// Binds the VAO
 	void Bind() {
 		glBindVertexArray(ID);
 	}
 
-	// Unbinds the VAO
 	void Unbind() {
 		glBindVertexArray(0);
 	}
 
-	// Deletes the VAO
 	void Delete() {
 		glDeleteVertexArrays(1, &ID);
 	}
