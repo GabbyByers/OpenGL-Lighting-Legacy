fc0ea447f4993391ab1d724c6940393bfd70c928
♡ gabby ♡ <81900622+GabrielBuckingham@users.noreply.github.com>
2023-11-10 14:33:04 -0800

    OpenGL, Lighting, W/Comments, No Libraries

--- VAO.h
+++ VAO.h
@@ -1,30 +1,38 @@
 #pragma once
+
 #include<glad/glad.h>
 #include"VBO.h"
 
-class VAO {
+class VAO
+{
 public:
+	// ID reference for the Vertex Array Object
 	GLuint ID;
-
+	
+	// Constructor that generates a VAO ID
 	VAO() {
 		glGenVertexArrays(1, &ID);
 	}
 
-	void LinkVBO(VBO& VBO, GLuint layout) {
+	// Links a VBO Attribute such as a position or color to the VAO
+	void LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset) {
 		VBO.Bind();
-		glVertexAttribPointer(layout, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		glVertexAttribPointer(layout, numComponents, type, GL_FALSE, stride, offset);
 		glEnableVertexAttribArray(layout);
 		VBO.Unbind();
 	}
 
+	// Binds the VAO
 	void Bind() {
 		glBindVertexArray(ID);
 	}
 
+	// Unbinds the VAO
 	void Unbind() {
 		glBindVertexArray(0);
 	}
 
+	// Deletes the VAO
 	void Delete() {
 		glDeleteVertexArrays(1, &ID);
 	}
